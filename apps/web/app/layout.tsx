fb0a7366faaa3b4b9b5df2f11a15a66e132520c7
Gustavo Bispo <gustavo.luiz.bispo.santos@gmail.com>
2026-08-14 21:01:04 -0300

    install shadcn and tailwind

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Inter, Nunito_Sans } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const nunitoSansHeading = Nunito_Sans({subsets:['latin'],variable:'--font-heading'});
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -22,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", inter.variable, nunitoSansHeading.variable)}>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
